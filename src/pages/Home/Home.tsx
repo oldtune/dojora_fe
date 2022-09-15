b3a9a8ffa58f6f9a6664a28d953d51b299f8ef1b
QuackQuack <vivaladaman@gmail.com>
2022-09-15 23:26:36 +0700

    modify file import path for many files

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,39 +1,36 @@
-import React, { useEffect, useState } from "react";
-import "./Home.less";
-import { Challenge } from "../../components/Challenge_List/Challenge";
-import { ChallengeList } from "../../components/Challenge_List/Challenge_List";
-import { ChallengeService } from "../../Services/Challenge.service";
-import {
-  ChallengeLookup,
-  ChallengeLookupSize,
-} from "../../components/Challenge_Lookup/Challenge_Lookup";
 import { Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import { Challenge } from "../../Components/Challenge_List/Challenge";
+import { ChallengeList } from "../../Components/Challenge_List/Challenge_List";
+import { ChallengeLookup, ChallengeLookupSize } from "../../Components/Challenge_Lookup/Challenge_Lookup";
+import { ChallengeService } from "../../Services/Challenge.service";
+import "./Home.less";
 
 export const Home: React.FC<{}> = (props: {}): JSX.Element => {
-  let [challenges, setChallenges] = useState([] as Challenge[]);
-  let [keyword, setKeyword] = useState("");
+    let [challenges, setChallenges] = useState([] as Challenge[]);
+    let [keyword, setKeyword] = useState("");
 
-  useEffect(() => {
-    ChallengeService.getList().subscribe((data) => {
-      setChallenges(data);
-    });
-  }, [keyword]);
+    useEffect(() => {
+        ChallengeService.getList().subscribe((data) => {
+            setChallenges(data);
+        });
+    }, [keyword]);
 
-  const searchButtonHandler: (value: string) => void = (value: string) => {
-    setKeyword(value);
-  };
+    const searchButtonHandler: (value: string) => void = (value: string) => {
+        setKeyword(value);
+    };
 
-  return (
-    <Row>
-      <Col md={{ span: 16, offset: 4 }} span={22} offset={1}>
-        <ChallengeLookup
-          {...{
-            onSearchClick: searchButtonHandler,
-            size: ChallengeLookupSize.Medium,
-          }}
-        />
-        <ChallengeList {...{ challenges }} />
-      </Col>
-    </Row>
-  );
+    return (
+        <Row>
+            <Col md={{ span: 16, offset: 4 }} span={22} offset={1}>
+                <ChallengeLookup
+                    {...{
+                        onSearchClick: searchButtonHandler,
+                        size: ChallengeLookupSize.Medium,
+                    }}
+                />
+                <ChallengeList {...{ challenges }} />
+            </Col>
+        </Row>
+    );
 };
